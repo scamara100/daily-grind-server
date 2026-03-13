a8857e7c0a2493cec37131836d10c02ee25ea1da
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-03-12 23:54:53 -0400

    added get resquests

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/contact.html"))
 })
 
 app.listen(port, () => {
